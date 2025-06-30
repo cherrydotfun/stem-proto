1f0cbc77274fad1c1166741972ff6abee040394c
e.taranin <macoeshka@gmail.com>
2025-06-30 16:05:24 +0300

    start creating reactive vue component

--- app/src/composables/stem.ts
+++ app/src/composables/stem.ts
@@ -1,157 +1,194 @@
-import { Solana, StemHelpers, Stem } from "../utils/stem";
-import { type AccountInfo, PublicKey } from "@solana/web3.js";
-import { ref, type ComputedRef, watchEffect, computed } from "vue";
+// import { Solana, StemHelpers, Stem } from "../utils/stem";
+// import { type AccountInfo, PublicKey } from "@solana/web3.js";
+// import { ref, type ComputedRef, watchEffect, computed } from "vue";
 
-let solana = new Solana();
+import type { Commitment, PublicKey } from "@solana/web3.js";
 
-export const initSolana = (rpcUrl: string = "http://localhost:8899") => {
-  solana = new Solana(rpcUrl);
+import { Connection } from "../utils/solana";
+
+type State = {
+  _connections: Record<string, Connection>;
 };
 
-export const getRawSolana = () => {
-  return solana;
+const state: State = {
+  _connections: {},
 };
 
-type RAccountInfo = {
-  accountInfo: AccountInfo<Buffer> | null;
-  isRegistered: boolean;
-};
-
-export const getAccountInfo = (publicKey: ComputedRef<PublicKey | null>) => {
-  const accountInfo = ref<RAccountInfo>({
-    accountInfo: null,
-    isRegistered: false,
-  });
-
-  const _loadData = async () => {
-    // console.log("Loading data", publicKey.value);
-    if (publicKey.value) {
-      const _tmp = await solana.getAccountInfo(publicKey.value);
-      accountInfo.value = {
-        accountInfo: _tmp,
-        isRegistered: _tmp ? true : false,
-      };
+// Connection params or exists connetion id
+type useConnectionParams =
+  | {
+      rpcUrl: string;
+      wsEndpoint: string;
+      commitment: Commitment;
     }
-  };
+  | string;
 
-  watchEffect(async (onCleanup) => {
-    let _interval = null;
-    if (publicKey.value) {
-      _loadData();
-      _interval = setInterval(_loadData, 5000);
-    } else {
-      accountInfo.value = {
-        accountInfo: null,
-        isRegistered: false,
-      };
-    }
-    onCleanup(() => {
-      console.log("Clearing interval");
-      if (_interval) {
-        clearInterval(_interval);
-      }
-    });
-  });
+export const getConnection = (params: useConnectionParams = "_default") => {
+  if (typeof params == "string") {
+    return state._connections[params];
+  }
 
-  return accountInfo;
+  state._connections["_default"] = new Connection(
+    params.rpcUrl,
+    params.wsEndpoint,
+    params.commitment
+  );
+
+  return state._connections["_default"];
 };
 
-function accountRef(timeout: number = 5000) {
-  return (publicKey: ComputedRef<PublicKey | null>) => {
-    const _account = ref<AccountInfo<Buffer> | null>(null);
+export const useStem = (connection: Connection, publicKey: PublicKey) => {};
 
-    const _loadData = async () => {
-      // console.log("Loading data", publicKey.value?.toBase58());
-      if (publicKey.value) {
-        const _tmp = await solana.getAccountInfo(publicKey.value);
-        _account.value = _tmp;
-      }
-    };
+// let solana = new Solana();
 
-    watchEffect((onCleanup) => {
-      let _interval = null;
-      if (publicKey.value) {
-        _loadData();
-        _interval = setInterval(_loadData, timeout);
-      }
-      onCleanup(() => {
-        // console.log("Clearing interval");
-        if (_interval) {
-          clearInterval(_interval);
-        }
-      });
-    });
+// export const initSolana = (rpcUrl: string = "http://localhost:8899") => {
+//   solana = new Solana(rpcUrl);
+// };
 
-    return _account;
-  };
-}
+// export const getRawSolana = () => {
+//   return solana;
+// };
 
-type Peer = {
-  pubkey: PublicKey;
-  status: number;
-};
+// type RAccountInfo = {
+//   accountInfo: AccountInfo<Buffer> | null;
+//   isRegistered: boolean;
+// };
 
-type WalletDescriptor = {
-  peers: Peer[];
-};
+// export const getAccountInfo = (publicKey: ComputedRef<PublicKey | null>) => {
+//   const accountInfo = ref<RAccountInfo>({
+//     accountInfo: null,
+//     isRegistered: false,
+//   });
 
-export const getWalletDescriptor = (
-  publicKey: ComputedRef<PublicKey | null>
-) => {
-  const pda = computed(() => StemHelpers.getDescriptorPda(publicKey.value));
+//   const _loadData = async () => {
+//     // console.log("Loading data", publicKey.value);
+//     if (publicKey.value) {
+//       const _tmp = await solana.getAccountInfo(publicKey.value);
+//       accountInfo.value = {
+//         accountInfo: _tmp,
+//         isRegistered: _tmp ? true : false,
+//       };
+//     }
+//   };
 
-  const _wallet = accountRef()(pda);
+//   watchEffect(async (onCleanup) => {
+//     let _interval = null;
+//     if (publicKey.value) {
+//       _loadData();
+//       _interval = setInterval(_loadData, 5000);
+//     } else {
+//       accountInfo.value = {
+//         accountInfo: null,
+//         isRegistered: false,
+//       };
+//     }
+//     onCleanup(() => {
+//       console.log("Clearing interval");
+//       if (_interval) {
+//         clearInterval(_interval);
+//       }
+//     });
+//   });
 
-  return computed(() => {
-    const _tmp = _wallet.value;
-    if (_tmp) {
-      // console.log(_tmp.data);
-      const data = Stem.deserializeDescriptor(
-        _tmp.data.subarray(8)
-      ) as WalletDescriptor;
-      // console.log(data);
-      for (const peer of data.peers) {
-        peer.pubkey = new PublicKey(peer.pubkey);
-      }
-      return {
-        data,
-        isRegistered: true,
-      };
-    } else {
-      return {
-        data: null,
-        isRegistered: false,
-      };
-    }
-  });
-};
+//   return accountInfo;
+// };
 
-export const getChatData = (
-  peer1: ComputedRef<PublicKey | null>,
-  peer2: ComputedRef<PublicKey | null>
-) => {
-  const pda = computed(() => {
-    if (!peer1.value || !peer2.value) {
-      return null;
-    }
-    return StemHelpers.getChatPda(peer1.value, peer2.value);
-  });
+// function accountRef(timeout: number = 5000) {
+//   return (publicKey: ComputedRef<PublicKey | null>) => {
+//     const _account = ref<AccountInfo<Buffer> | null>(null);
 
-  console.log("chat pda", pda.value?.toBase58());
-  const _chat = accountRef(5000)(pda);
+//     const _loadData = async () => {
+//       // console.log("Loading data", publicKey.value?.toBase58());
+//       if (publicKey.value) {
+//         const _tmp = await solana.getAccountInfo(publicKey.value);
+//         _account.value = _tmp;
+//       }
+//     };
 
-  return computed(() => {
-    const _tmp = _chat.value;
-    if (_tmp) {
-      const data = Stem.deserializeChat(_tmp.data.subarray(8));
-      if (!data) {
-        throw new Error("Failed to deserialize chat");
-      }
-      console.log("chat data", data);
-      data.wallets[0] = new PublicKey(data.wallets[0]);
-      data.wallets[1] = new PublicKey(data.wallets[1]);
-      return data;
-    }
-    return null;
-  });
-};
+//     watchEffect((onCleanup) => {
+//       let _interval = null;
+//       if (publicKey.value) {
+//         _loadData();
+//         _interval = setInterval(_loadData, timeout);
+//       }
+//       onCleanup(() => {
+//         // console.log("Clearing interval");
+//         if (_interval) {
+//           clearInterval(_interval);
+//         }
+//       });
+//     });
+
+//     return _account;
+//   };
+// }
+
+// type Peer = {
+//   pubkey: PublicKey;
+//   status: number;
+// };
+
+// type WalletDescriptor = {
+//   peers: Peer[];
+// };
+
+// export const getWalletDescriptor = (
+//   publicKey: ComputedRef<PublicKey | null>
+// ) => {
+//   const pda = computed(() => StemHelpers.getDescriptorPda(publicKey.value));
+
+//   const _wallet = accountRef()(pda);
+
+//   return computed(() => {
+//     const _tmp = _wallet.value;
+//     if (_tmp) {
+//       // console.log(_tmp.data);
+//       const data = Stem.deserializeDescriptor(
+//         _tmp.data.subarray(8)
+//       ) as WalletDescriptor;
+//       // console.log(data);
+//       for (const peer of data.peers) {
+//         peer.pubkey = new PublicKey(peer.pubkey);
+//       }
+//       return {
+//         data,
+//         isRegistered: true,
+//       };
+//     } else {
+//       return {
+//         data: null,
+//         isRegistered: false,
+//       };
+//     }
+//   });
+// };
+
+// export const getChatData = (
+//   peer1: ComputedRef<PublicKey | null>,
+//   peer2: ComputedRef<PublicKey | null>
+// ) => {
+//   const pda = computed(() => {
+//     if (!peer1.value || !peer2.value) {
+//       return null;
+//     }
+//     return StemHelpers.getChatPda(peer1.value, peer2.value);
+//   });
+
+//   console.log("chat pda", pda.value?.toBase58());
+//   const _chat = accountRef(5000)(pda);
+
+//   return computed(() => {
+//     const _tmp = _chat.value;
+//     if (_tmp) {
+//       const data = Stem.deserializeChat(_tmp.data.subarray(8));
+//       if (!data) {
+//         throw new Error("Failed to deserialize chat");
+//       }
+//       console.log("chat data", data);
+//       data.wallets[0] = new PublicKey(data.wallets[0]);
+//       data.wallets[1] = new PublicKey(data.wallets[1]);
+//       return data;
+//     }
+//     return null;
+//   });
+// };
